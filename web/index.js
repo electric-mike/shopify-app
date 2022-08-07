aae0127c79413c0474f6f25f79e8ddcd6e4fea7c
electric-mike <66960109+electric-mike@users.noreply.github.com>
2022-08-07 13:47:27 -0400

    Ooops; bad code

--- web/index.js
+++ web/index.js
@@ -42,7 +42,7 @@ Shopify.Context.initialize({
   // This should be replaced with your preferred storage strategy
   // SESSION_STORAGE: new Shopify.Session.SQLiteSessionStorage(DB_PATH),
   SESSION_STORAGE: process.env.NODE_ENV !== 'production' ? 
-    new Shopify.Session.MongoDBSessionStorage("mongodb+srv://electriceye:DU8VFxy0othLzUO4@cluster0.m9d6cmr.mongodb.net/?retryWrites=true&w=majority", "database"),
+    new Shopify.Session.MongoDBSessionStorage("mongodb+srv://electriceye:DU8VFxy0othLzUO4@cluster0.m9d6cmr.mongodb.net/?retryWrites=true&w=majority", "database")
     : new Shopify.Session.SQLiteSessionStorage(DB_PATH)
 });
 
